f859f91f9771c4fc046fbfd3b658b8805c2ff6b6
Eden <edendamessa@gmail.com>
2022-07-01 14:01:58 -0700

    print fc

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 ( success)
+ * betty style doc for function main goes there
+ */
+
+int main(void)
+
+{
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
