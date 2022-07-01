e5d073ed34f122a5311fe330734143ba88614ac4
Eden <edendamessa@gmail.com>
2022-07-01 13:53:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -Entry point
- * Return: Always 0 (success)
- * betty style doc for function main goes there
- */
- int main(void)
+* main -Entry point
+* Rturn: Always 0 (success)
+* betty style doc for function main goes there
+*/
+int main(void)
 {
-	  puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
