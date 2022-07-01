a4b2674444f47f9ebf603cc0643d458854cb9835
Eden <edendamessa@gmail.com>
2022-07-01 13:10:51 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,29 @@
-eden
+#include <stdio.h>
+
+/**
+ * main - testing lowercase alphabet.
+ * Return: Always 0 (Success)
+ * betty style doc for function main goes there
+ */
+
+int main(void)
+
+{
+
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+
+	for (i = 0; i < 26; i++)
+
+	{
+
+		putchar(alpha[i]);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
