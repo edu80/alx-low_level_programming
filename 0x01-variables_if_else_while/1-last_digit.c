ca1b0ed4a497514b9889babfd5184682ab44eb8a
Eden <edendamessa@gmail.com>
2022-07-01 13:05:32 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,15 +3,10 @@
 #include <time.h>
 
 /**
- *
  * main - Determines if a number n is either greter than 5, less than 6 /= 0
- *
  * Return: Always 0 (Success)
- *
  * betty style doc for function main goes there
- *
  * more headers goes there
- *
  */
 
 int main(void)
