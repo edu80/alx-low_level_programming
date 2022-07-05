d420eb3972459bca72bde637f5276bd8308ec76d
Eden <edendamessa@gmail.com>
2022-07-05 16:30:27 -0700

    putchar

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main calls function
+ *
+ * return:0
+ */
+int main(void)
+{
+  printc();
+  return (0);
+}
---
+++ 0x02-functions_nested_loops/printc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print _putchar thrn a new lin *
+ * Return:0  Always (SUCCESS)
+ * betty style doc for function main goes there
+ */
+
+void printc(void)
+
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+}
