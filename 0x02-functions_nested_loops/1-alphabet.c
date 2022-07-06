ae2704a80ee1e86d046d35e942bc6b7cd5bae2f5
Eden <edendamessa@gmail.com>
2022-07-05 17:17:53 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print lowercase alphabet
+ * Return:0  Always (SUCCESS)
+ * betty style doc for function main goes there
+ */
+
+void print_alphabet(void)
+
+{
+int i;
+for (i = 97; i <= 122; i++)
+ {
+  _putchar(i);
+ }
+/* using ASCII valuse in the code above */
+_putchar('\n');
+
+}
