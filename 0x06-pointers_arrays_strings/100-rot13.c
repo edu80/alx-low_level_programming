67b92fcd3ccddde1a4c61bb91e0327cbe458e8ea
Eden <edendamessa@gmail.com>
2022-07-14 16:32:17 -0700

    advanced pointer task

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,28 +1,42 @@
-#include "main.h"
+#include "holberton.h"
 /**
-* rot13 - encodes a string using rot13
-* @s: input string.
-* Return: the pointer to dest.
-* betty style doc for function main goes there
+* rot13 - Encodes a string using rot13.
+* @str: The string to be encoded.
+* Return: A pointer to the encoded string.
 */
 
-char *rot13(char *s)
+char *rot13(char *str)
 {
-int encode = 0, i;
-char alph[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-while (*(s + encode) != '\0')
+int indx1 = 0, indx2;
+char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+'G', 'H', 'I', 'J', 'K', 'L',
+'M', 'N', 'O', 'P', 'Q', 'R',
+'S', 'T', 'U', 'V', 'W', 'X',
+'Y', 'Z', 'a', 'b', 'c', 'd',
+'e', 'f', 'g', 'h', 'i', 'j',
+'k', 'l', 'm', 'n', 'o', 'p',
+'q', 'r', 's', 't', 'u', 'v',
+'w', 'x', 'y', 'z'};
+char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+'T', 'U', 'V', 'W', 'X', 'Y',
+'Z', 'A', 'B', 'C', 'D', 'E',
+'F', 'G', 'H', 'I', 'J', 'K',
+'L', 'M', 'n', 'o', 'p', 'q',
+'r', 's', 't', 'u', 'v', 'w',
+'x', 'y', 'z', 'a', 'b', 'c',
+'d', 'e', 'f', 'g', 'h', 'i',
+'j', 'k', 'l', 'm'};
+while (str[indx1])
 {
-for (i = 0; i < 52; i++)
+for (indx2 = 0; indx2 < 52; indx2++)
 {
-if (*(s + encode) == alph[i])
+if (str[indx1] == alphabet[indx2])
 {
-*(s + encode) = rot13[i];
+str[indx1] = rot13key[indx2];
 break;
 }
 }
-encode++;
+indx1++;
 }
-return (s);
-}
+return (str);
 }
