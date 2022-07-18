f1bbc52d002e8be7232175b77a8029c2f5a95009
Eden <edendamessa@gmail.com>
2022-07-18 12:55:31 -0700

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
 * _memset - fills memory with constant byte.
 * @s: array of char
@@ -6,14 +7,15 @@
 * @n: number of bytes
 * Return:(s)
 */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
-int i=0;
-while (i<n)
+char *p = s;
+while (n > 0)
 {
-s[i]=b;
-i++;
+*p = b;
+p++;
+n--;
 }
-return(s);
+return (s);
 }
