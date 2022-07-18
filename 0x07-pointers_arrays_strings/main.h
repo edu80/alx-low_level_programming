2401c51b6e35923f21b06be4f68fadad61e73526
Eden <edendamessa@gmail.com>
2022-07-18 10:44:24 -0700

    task0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,14 @@
-/* MAIN_H */
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+* File: holberton.h
+* Auth: Firdaus
+* Desc: Header file containing prototypes for all functions
+* used in the 0x07-pointers_arrays_strings directory.
+*/
+
+#include <stddef.h>
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
@@ -9,3 +19,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
+#endif
