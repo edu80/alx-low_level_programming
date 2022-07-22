a744a713a4efe386bb4ac09e477df3456ae47039
Eden <edendamessa@gmail.com>
2022-07-22 12:19:58 -0700

    in C argc,argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
+
 /**
 * main - Program that prints the number of arguments passed into it
 * @argc: - Int of arguments passed into program including command
@@ -7,17 +9,20 @@
 * Return: 0
 */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-int a, b, c;
-if (argc != 3)
+int i, j;
+if (argc == 1 || argc == 2)
 {
 printf("Error\n");
 return (1);
 }
-a = atoi(*(argv + 1));
-b = atoi(*(argv + 2));
-c = a * b;
-printf("%d\n", c);
+else
+{
+j = 1;
+for (i = 1; i < 3; i++)
+j *= atoi(argv[i]);
+printf("%d\n", j);
+}
 return (0);
 }
