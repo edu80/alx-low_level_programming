dde748cf604b6f7a7edf49390a4d5b18a7cebf18
Eden <edendamessa@gmail.com>
2022-07-26 05:16:37 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,55 +1,48 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
-*_strlen - counts and returns string length
-* @s: that's the string
-* Return: the length
-*/
-int _strlen(char *s)
-{
-int counter = 0;
-if (!*s)
-return (0);
-while (*s)
-{
-counter++;
-s++;
-}
-return (counter);
-}
-/**
-* str_concat - concatenates two strings
-* @s1: one string
-* @s2: the other
-* Return: pointer to cat string
+* str_concat - concatenate two strings
+* @s1: a pointer to the first string
+* @s2: a pointer to the second string
+* Return: NULL if memory allocation fails,
+* otherwise a pointer to a the new string
 */
 
 char *str_concat(char *s1, char *s2)
 {
-char *new;
-unsigned int i;
-unsigned int j;
-int total = 0;
-if (!s1)
+unsigned int len1 = 0;
+unsigned int len2 = 0;
+char *cat;
+if (s1)
+{
+while (s1[len1])
+++len1;
+}
+else
+{
 s1 = "";
-if (!s2)
+}
+if (s2)
+{
+while (s2[len2])
+++len2;
+}
+else
+{
 s2 = "";
-total += _strlen(s1) + _strlen(s2);
-new = malloc((total * sizeof(char)) + 1);
-if (new == NULL)
-{
+}
+cat = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
+if (!cat)
 return (NULL);
-}
-for (i = 0; s1[i]; i++)
+if (s1)
 {
-new[i] = s1[i];
+for (len1 = 0; s1[len1]; ++len1)
+cat[len1] = s1[len1];
 }
-for (j = 0; s2[j]; j++, i++)
+if (s2)
 {
-new[i] = s2[j];
+for (len2 = 0; s2[len2]; ++len2)
+cat[len1 + len2] = s2[len2];
 }
-new[i] = '\0';
-return (new);
+cat[len1 + len2] = '\0';
+return (cat);
 }
